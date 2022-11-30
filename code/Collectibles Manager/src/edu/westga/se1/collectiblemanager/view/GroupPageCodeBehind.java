4813aadee9c68f2a187225c2f1095be3c2064597
racheleckle <116831464+racheleckle@users.noreply.github.com>
2022-11-30 18:58:56 -0500

    Add GroupPageGUI.

---
+++ code/Collectibles Manager/src/edu/westga/se1/collectiblemanager/view/GroupPageCodeBehind.java
@@ -0,0 +1,23 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class GroupPageCodeBehind {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private ListView<Group> groupsListView;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button viewButton;
+
+}
