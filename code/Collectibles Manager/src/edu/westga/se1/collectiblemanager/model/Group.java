690b55e197b036394765f9ded0465c7d156dee15
racheleckle <116831464+racheleckle@users.noreply.github.com>
2022-12-01 14:12:07 -0500

    Implement Group class functionality.

--- code/Collectibles Manager/src/edu/westga/se1/collectiblemanager/model/Group.java
+++ code/Collectibles Manager/src/edu/westga/se1/collectiblemanager/model/Group.java
@@ -4,24 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Michael Lee
- *
+ * The Group class.
+ * 
+ * @author Michael Lee & Rachel Eckleberry
+ * @version Fall 2022
  */
 public class Group {
+
 	private String name;
 	private List<Collectible> collectibles;
-	
+
 	/**
 	 * Constructor for group object
+	 * 
+	 * @precondition name != null && !name.isEmpty
+	 * @postcondition none
+	 * 
 	 * @param name the name of the group
 	 */
 	public Group(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("The group name cannot be null.");
+		}
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("The group name cannot be empty.");
+		}
+
 		this.name = name;
 		this.collectibles = new ArrayList<Collectible>();
 	}
 
 	/**
 	 * Gets the name
+	 * 
 	 * @return the name
 	 */
 	public String getName() {
@@ -30,6 +45,7 @@ public class Group {
 
 	/**
 	 * Sets the name
+	 * 
 	 * @param name the name to set
 	 */
 	public void setName(String name) {
@@ -38,10 +54,56 @@ public class Group {
 
 	/**
 	 * Gets the collectibles
+	 * 
 	 * @return the list of collectibles
 	 */
 	public List<Collectible> getCollectibles() {
 		return this.collectibles;
 	}
 
+	/**
+	 * Adds a collectible to the group.
+	 * 
+	 * @precondition collectible != null
+	 * @postcondition the collectible is added
+	 * 
+	 * @param collectible the collectible to add
+	 * @return true if the collectible was added, false otherwise
+	 */
+	public boolean addCollectible(Collectible collectible) {
+		if (collectible == null) {
+			throw new IllegalArgumentException("The collectible cannot be null.");
+		}
+
+		int groupSize = this.collectibles.size();
+		this.collectibles.add(collectible);
+
+		if (this.collectibles.size() == groupSize + 1) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Removes a collectible from the group at the specified index.
+	 * 
+	 * @precondition this.collectibles.size > index >= 0
+	 * @postcondition the collectible is removed
+	 * 
+	 * @param index the index of the collectible to remove
+	 * @return true if the collectible was removed, false otherwise
+	 */
+	public boolean removeCollectible(int index) {
+		if (index < 0 || index >= this.collectibles.size()) {
+			throw new IllegalArgumentException("The index must be within the bounds of the group's collection.");
+		}
+
+		int groupSize = this.collectibles.size();
+		this.collectibles.remove(index);
+
+		if (this.collectibles.size() == groupSize - 1) {
+			return true;
+		}
+		return false;
+	}
 }
--- code/Collectibles Manager/src/edu/westga/se1/collectiblemanager/view/GroupPageCodeBehind.java
+++ code/Collectibles Manager/src/edu/westga/se1/collectiblemanager/view/GroupPageCodeBehind.java
@@ -1,6 +1,9 @@
 package edu.westga.se1.collectiblemanager.view;
 
-import edu.westga.se1.collectiblemanager.model.CollectibleGroup;
+import edu.westga.se1.collectiblemanager.model.Group;
+import edu.westga.se1.collectiblemanager.viewmodel.GroupViewModel;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -8,19 +11,35 @@ import javafx.scene.control.TextField;
 
 public class GroupPageCodeBehind {
 
-    @FXML
-    private Button addButton;
+	@FXML
+	private Button addButton;
 
-    @FXML
-    private ListView<CollectibleGroup> groupsListView;
+	@FXML
+	private ListView<Group> groupsListView;
 
-    @FXML
-    private TextField nameTextField;
+	@FXML
+	private TextField nameTextField;
 
-    @FXML
-    private Button removeButton;
+	@FXML
+	private Button removeButton;
 
-    @FXML
-    private Button viewButton;
+	@FXML
+	private Button viewButton;
 
+	private ObjectProperty<Group> selectedProperty;
+	private GroupViewModel viewModel;
+
+	public GroupPageCodeBehind() {
+		this.viewModel = new GroupViewModel();
+		this.selectedProperty = new SimpleObjectProperty<Group>();
+	}
+
+	@FXML
+	private void initialize() {
+		this.bindToViewModel();
+	}
+
+	private void bindToViewModel() {
+
+	}
 }
